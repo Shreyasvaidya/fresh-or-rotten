8d592d723ccdeb3e3b12f3ea78e17c40cfedb778
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2022-05-18 19:36:34 +0530

    Add files via upload

---
+++ image_classification.py
@@ -0,0 +1,39 @@
+import keras
+from PIL import Image
+from PIL import ImageOps
+import cv2
+import numpy as np
+#from rembg import remove
+
+def machine_classification(img,weights_file ):
+    # Load the model
+    model = keras.models.load_model(weights_file)
+
+    # Create the array of the right shape to feed into the keras model
+    data = np.ndarray(shape=(1, 45, 45, 3), dtype=np.float32)
+    image = img
+    #image sizing
+    size = (45, 45)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    #image=remove(image)
+    try:
+        image.save("geeks.jpeg")
+	 #turn the image into a numpy array
+        img_array=np.array(cv2.imread("geeks.jpeg"))
+    except:
+        image.save("geeks.png")
+        img_array=np.array(cv2.imread("geeks.png"))    
+   
+  
+        
+
+    array=cv2.resize(img_array,size)
+    # Normalize the image
+    normalized_image_array = array/255 
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    return np.argmax(prediction) # return position of the highest probability
